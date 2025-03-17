949c25bd9d2a3e045388238983805735e37c1381
Suphachoke ASUS-ROG <suphachoke.son@gmail.com>
2025-03-17 13:47:18 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
-      <Route path="/auth/*" element={<Auth />} />
       <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
     </Routes>
   );
